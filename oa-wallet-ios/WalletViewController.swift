ed076aa14043b0512e8c7fc0bcbbc0b27550e965
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-02 18:02:50 +0800

    chore: cleanup

--- oa-wallet-ios/WalletViewController.swift
+++ oa-wallet-ios/WalletViewController.swift
@@ -30,26 +30,24 @@ class WalletViewController: UIViewController {
         let fileName = url.lastPathComponent
         let alert = UIAlertController(title: fileName, message: nil, preferredStyle: .actionSheet)
         
-        alert.addAction(UIAlertAction(title: "Save to wallet", style: .default , handler:{ (UIAlertAction)in
+        alert.addAction(UIAlertAction(title: "Save to wallet", style: .default , handler:{ (UIAlertAction) in
             let outputUrl = getDocumentsDirectory().appendingPathComponent(fileName)
             do {
                 try _ = FileManager.default.replaceItemAt(outputUrl, withItemAt: url)
-                let input = try String(contentsOf: outputUrl)
-                print(input)
             } catch {
                 print(error.localizedDescription)
             }
         }))
         
-        alert.addAction(UIAlertAction(title: "Verify", style: .default , handler:{ (UIAlertAction)in
+        alert.addAction(UIAlertAction(title: "Verify", style: .default , handler:{ (UIAlertAction) in
            
         }))
         
-        alert.addAction(UIAlertAction(title: "View", style: .default , handler:{ (UIAlertAction)in
+        alert.addAction(UIAlertAction(title: "View", style: .default , handler:{ (UIAlertAction) in
             
         }))
         
-        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction) in
             
         }))
         
