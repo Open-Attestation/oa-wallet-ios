734511888c5ebb54bbdbd9eee65d709d8faf52a2
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-02 14:03:33 +0800

    Initial Commit

---
+++ oa-wallet-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oa-wallet-ios
+//
+//  Created by Tan Cher Shen on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
