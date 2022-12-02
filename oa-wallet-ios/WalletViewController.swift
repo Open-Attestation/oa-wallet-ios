d9da66ecf95544b18187f883ead33a8fe0b58d7f
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-02 17:39:25 +0800

    feat: display action sheet for import actions

--- oa-wallet-ios/WalletViewController.swift
+++ oa-wallet-ios/WalletViewController.swift
@@ -25,8 +25,37 @@ class WalletViewController: UIViewController {
         documentPicker.modalPresentationStyle = .fullScreen
         present(documentPicker, animated: true, completion: nil)
     }
+    
+    func presentImportActions(url: URL) {
+        let fileName = url.lastPathComponent
+        let alert = UIAlertController(title: fileName, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Save to wallet", style: .default , handler:{ (UIAlertAction)in
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Verify", style: .default , handler:{ (UIAlertAction)in
+           
+        }))
+        
+        alert.addAction(UIAlertAction(title: "View", style: .default , handler:{ (UIAlertAction)in
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
+            
+        }))
+        
+        alert.popoverPresentationController?.sourceView = self.view
+        self.present(alert, animated: true, completion: {
+            
+        })
+    }
 }
 
 extension WalletViewController: UIDocumentPickerDelegate {
-    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else { return }
+        presentImportActions(url: url)
+    }
 }
